659e794fc89f007f44aa24ec720b1a0ca841a66d
pedrocunial <pedrocc4@al.insper.edu.br>
2016-07-15 13:33:37 -0300

    parsing through json

--- NewBoston/JSONExample/app.js
+++ NewBoston/JSONExample/app.js
@@ -1,2 +1,20 @@
-    // for(var item in app.locals.videodata.categories) {
-    //     response.write(item.categoryName);
+var fs = require('fs');
+var obj;
+
+// fs.readFile('videodata', 'utf8', function(err, data) {
+//     if(err) {
+//         throw err;
+//     }
+
+//     obj = require('./videodata.json'); 
+  
+// });
+
+var json = require('./videodata.json');
+
+// This is the way of parsing through a json
+// with a "foreach" like loop
+for(var category in json.categories) {
+    // if(err) throw err;
+    console.log(json.categories[category].categoryID);
+}
